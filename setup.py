bd8756b96954058e23fc2c6313138b5bdcd5c1bc
Felix Hummel <felix@felixhummel.de>
2012-11-13 19:52:13 +0100

    bump version for release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='pwman',
-      version='0.0.3',
+      version='1.0.0',
       description='Simple command line client to manage nginx basic auth files. Think htpasswd (but not quite) as lib. Writes SSHA hashes.',
       author='Felix Hummel',
       author_email='felix@felixhummel.de',
